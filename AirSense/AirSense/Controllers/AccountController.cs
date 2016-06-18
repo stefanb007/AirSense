cc6d5a6505be5033019bfd684a629da80ba42009
marti <marti@DESKTOP-UD3FDFD>
2016-06-19 00:55:31 +0200

    add WORDPRESS, hash and salt

--- AirSense/AirSense/Controllers/AccountController.cs
+++ AirSense/AirSense/Controllers/AccountController.cs
@@ -8,11 +8,41 @@ using FluentNHibernate;
 using NHibernate;
 using AirSense.Mappings;
 using NHibernate.Linq;
+using System.Text;
 
 namespace AirSense.Controllers
 {
+    //ヽ༼ຈل͜ຈ༽ﾉ WORDPRESS or RIOT ヽ༼ຈل͜ຈ༽ﾉ
+
     public class AccountController : Controller
     {
+        public static string ByteArrayToHexString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+            return hex.ToString();
+        }
+
+        public String CreateSalt(int sizeSalt)
+        {
+            var rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            var buff = new byte[sizeSalt];
+            rng.GetBytes(buff);
+            return Convert.ToBase64String(buff);
+        }
+
+        public String GenerateSHA256(String input, String salt)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(input + salt);
+            System.Security.Cryptography.SHA256Managed sha256hashstring = new System.Security.Cryptography.SHA256Managed();
+            byte[] hash = sha256hashstring.ComputeHash(bytes);
+
+            return ByteArrayToHexString(hash);
+        }
+
         [HttpGet]
         public ActionResult Login()
         {
