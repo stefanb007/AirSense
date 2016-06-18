b38a6ded288c582bd0ea0189354f55102a535fb8
Stefan Baltov <stefan.baltov7@gmail.com>
2016-06-18 14:45:23 +0200

    add: UserViewModel class

---
+++ AirSense/AirSense/Models/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentValidation;
+
+namespace AirSense.Models
+{
+    []
+    public class UserViewModel
+    {
+        public string Name;
+        public string Surname;
+        public string Password;
+    }
+
+    //public class
+}
